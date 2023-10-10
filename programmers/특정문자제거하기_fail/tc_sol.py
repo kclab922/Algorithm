3d4b1cee97c8bc2891ed03f0f825e3db5b464572
HaeunChoi <kclab922@gmail.com>
2023-10-10 15:48:40 +0900

    update

---
+++ programmers/특정문자제거하기_fail/tc_sol.py
@@ -0,0 +1,18 @@
+# #방법1 
+# def solution(my_string, letter):
+#     anwer = ''
+#     answer = my_string.replace(letter, '')
+
+#     return answer
+
+#방법2 응용
+def solution(my_string, letter):
+    answer = ''
+    for string in my_string:
+        if string != letter:
+            answer += string
+
+    return answer
+
+print(solution("abcdef", "f"))
+print(solution("BCBdbe", "B"))
