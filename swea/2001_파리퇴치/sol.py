1c6e726a68f3418e2a633e20b843e5aa6579fa33
HaeunChoi <kclab922@gmail.com>
2023-10-18 16:31:51 +0900

    update

--- swea/2001_파리퇴치/sol.py
+++ swea/2001_파리퇴치/sol.py
@@ -1,7 +1,18 @@
 import sys
 sys.stdin = open('input.txt')
 
-# input 값들 배정
+# input 값들 배정 
 T = int(input())
+
 for tc in range(1, T+1):
+    # N: 전체 보드의 길이
+    # M: 파리채의 길이
     N, M = list(map(int, input().split()))
+
+    matrix = []
+
+    for _ in range(N):
+        row = list(map(int, input().split()))
+        matrix.append(row)
+
+        
