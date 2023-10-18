94cfa8699de6118a0b423bbb97b2c41b5d4981fe
HaeunChoi <kclab922@gmail.com>
2023-10-18 16:41:37 +0900

    update

--- swea/2001_파리퇴치/sol.py
+++ swea/2001_파리퇴치/sol.py
@@ -15,4 +15,24 @@ for tc in range(1, T+1):
         row = list(map(int, input().split()))
         matrix.append(row)
 
-        
+    # print(matrix)
+
+    total = 0
+    # 파리채를 그리기 위한 기준점 잡는 반복문
+    for i in range(N-M+1):
+        for j in range(N-M+1):
+
+            temp = 0
+            # 파리채를 그리는 반복문
+            for row in range(M):
+                for col in range(M):
+                    temp += matrix[i+row][j+col]
+
+            if total < temp:
+                total = temp
+    
+    print(f'#{tc} {total}')
+
+
+
+
