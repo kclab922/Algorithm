a0b5829067e9a9db40c7f78f6b852ef113787c33
HaeunChoi <kclab922@gmail.com>
2023-10-21 13:55:54 +0900

    update

--- programmers/level_0/배열원소의길이/sol.py
+++ programmers/level_0/배열원소의길이/sol.py
@@ -1,2 +1,9 @@
+# 풀이1
 def solution(strlist):
-    
+    length = []
+    for i in range(len(strlist)):
+        length.append(len(strlist[i]))
+    return length
+
+print(solution(["We", "are", "the", "world!"]))
+print(solution(["I", "Love", "Programmers."]))
