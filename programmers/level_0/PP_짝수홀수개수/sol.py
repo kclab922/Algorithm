22a09dc7a75aa997dadd73e01e4a2fa1d26275ca
HaeunChoi <kclab922@gmail.com>
2023-10-23 18:04:06 +0900

    update

--- programmers/level_0/PP_짝수홀수개수/sol.py
+++ programmers/level_0/PP_짝수홀수개수/sol.py
@@ -5,7 +5,7 @@ def solution(num_list):
             my_list[0] += 1
         else:
             my_list[1] += 1
-    return
+    return my_list
 
 print(solution([1, 2, 3, 4, 5]))
 print(solution([1, 3, 5, 7]))
