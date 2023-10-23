09550a7a2e4efc2dbfa66ead73c4dcd27414d998
HaeunChoi <kclab922@gmail.com>
2023-10-23 17:57:49 +0900

    update

---
+++ programmers/level_0/짝수홀수개수/sol.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    my_list = [0, 0]
+    for num in num_list:
+        if num % 2 == 0:
+            my_list[0] += 1
+        else:
+            my_list[1] += 1
+    return
+
+print(solution([1, 2, 3, 4, 5]))
+print(solution([1, 3, 5, 7]))
