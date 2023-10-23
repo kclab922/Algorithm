785f6a37974d27c50f0a0bb010ad6cb1ad023a02
HaeunChoi <kclab922@gmail.com>
2023-10-23 17:44:03 +0900

    update

--- programmers/level_0/P_문자열안에문자열/sol.py
+++ programmers/level_0/P_문자열안에문자열/sol.py
@@ -10,9 +10,6 @@ def solution(str1, str2):
         answer = 2
     return answer
 
-
-
-
 print(solution("ab6CDE443fgh22iJKlmn1o", "6CD"))
 print(solution("ppprrrogrammers", "pppp"))
 print(solution("AbcAbcA", "AAA"))
