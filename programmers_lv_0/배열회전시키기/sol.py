33b811b2c82949732c2e87da3f7b4dd10fd8a372
HaeunChoi <kclab922@gmail.com>
2023-10-29 18:50:39 +0900

    pro_배열회전시키기

--- programmers_lv_0/배열회전시키기/sol.py
+++ programmers_lv_0/배열회전시키기/sol.py
@@ -1,6 +1,10 @@
 def solution(numbers, direction):
     answer = []
-    
+    if direction == 'right':
+        answer = [numbers.pop()] + numbers
+    else:
+        answer = numbers + [numbers.pop(0)]
+    return answer
 
 print(solution([1, 2, 3], "right"))
-print(solution([4, 455, 6, 4, -1, 45, 6], "left"))
+print(solution([4, 455, 6, 4, -1, 45, 6], "left"))``
