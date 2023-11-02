d95f87c89714b8baddf56c70693db8cfe2b3f34f
HaeunChoi <kclab922@gmail.com>
2023-11-02 18:55:12 +0900

    pro_최빈값구하기

--- programmers_lv_0/최빈값구하기/sol.py
+++ programmers_lv_0/최빈값구하기/sol.py
@@ -13,15 +13,15 @@
 
 
 # 다른 풀이
-# 숫자 하나씩 돌아가며 삭제 => 마지막에 남는 애가 최빈값 /  
+# 숫자 하나씩 돌아가며 삭제 => 마지막에 남는 애가 최빈값 
 # .remove(): list method
+# for문 반복시 원본에 뭔가를 씌우면, 이는 복사본으로 처리됨 -> 원본이 손상되어도 복사본은 유지
 def solution(array):
     while len(array) != 0:
         for i, a in enumerate(set(array)):
-    #         array.remove(a)
-    #     if i == 0: return a
-    # return -1
-            return i,a
+            array.remove(a)
+        if i == 0: return a
+    return -1
 
 print(solution([1, 2, 3, 3, 3, 4]))
 print(solution([1, 1, 2, 2]))
