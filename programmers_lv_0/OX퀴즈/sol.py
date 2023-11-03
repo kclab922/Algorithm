beda8f641838ffa3614ee21f5f46c53f8b49dfc5
HaeunChoi <kclab922@gmail.com>
2023-11-04 00:11:57 +0900

    pro_OX퀴즈

--- programmers_lv_0/OX퀴즈/sol.py
+++ programmers_lv_0/OX퀴즈/sol.py
@@ -1,9 +1,19 @@
 def solution(quiz):
-    result = []
+    answer = []
     for q in quiz:
-        list(q).remove(' ')
-
-    return 
+        q = q.split(' ') # ['5', '+', '6', '=', '11']
+        cal = int(q[0])
+        for c in q:
+            if c == '+':
+                cal += int(q[q.index(c) + 1])
+            elif c == '-':
+                cal -= int(q[q.index(c) + 1])
+        if cal == int(q[-1]):
+            answer.append('O')
+        else:
+            answer.append('X')
+                
+    return answer
 
 
 print(solution(["3 - 4 = -3", "5 + 6 = 11"]))
