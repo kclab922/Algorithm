f9beb2e8b33cd1232fd9d01bb95d0118853dab35
HaeunChoi <kclab922@gmail.com>
2023-11-04 19:19:24 +0900

    pro_치킨쿠폰

---
+++ programmers_lv_0/P_치킨쿠폰/sol.py
@@ -0,0 +1,31 @@
+# 내 코드
+# 풀이과정
+# 1. '쿠폰'과 '서비스'를 변수로 이용 (쿠폰 기본값 = chicken: 첫 번째 쿠폰 개수는 chicken 개수와 같으므로 이렇게 시작)
+# 2. 쿠폰 개수를 10으로 나누어 => 몫은 서비스에 누적하고, 몫과 나머지를 더해 쿠폰 개수 도출 
+# 3. 쿠폰 개수가 10개 이상인 동안은 추가 서비스가 발생하므로, 이 과정을 계속 반복 
+# 4. 쿠폰 개수가 9개 이하로 떨어지면 더이상의 추가 서비스는 없으므로 중단
+# divmod 호진님 헌정 문제
+
+def solution(chicken):
+    coupon = chicken
+    service = 0
+
+    while coupon >= 10:
+        a, b = divmod(coupon, 10)
+        service += a
+        coupon = a + b
+    
+    return service
+
+
+# 참고 코드 1
+# 서비스 개수는 10%의 10%의 10%의 10%...를 계속 더해가는 과정이므로! + int 씌워서 정수만 추출
+def solution(chicken):
+    return int(chicken*0.11111111111)
+
+# 참고 코드 2
+def solution(chicken):
+    return (max(chicken,1)-1)//9
+
+print(solution(100))
+print(solution(1081))
--- programmers_lv_0/다항식더하기/sol.py
+++ programmers_lv_0/다항식더하기/sol.py
@@ -1,17 +1,31 @@
-def solution(polynomial):
-    # v = ''
-    # n = 0
-    p = (polynomial.replace('x', '*1')).split()
+def solution(pol):
+    pol = pol.split(' ') # ['3x', '+', '7', '+', 'x']
+    cal_x = 0
+    cal_n = 0
+    for p in pol:
+        if 'x' in p:
+            if len(p) == 1:
+                cal_x += 1
+            else:
+                cal_x += int(p[:-1])
+        elif p =='+':
+            continue
+        else:
+            cal_n += int(p)
     
-    # for i in range(0, len(p), 2):
-    #     if p[i].isdigit():
-    #         n += p[i]
-    #     else:
+    answer_a = str(cal_x) + 'x' 
+    if cal_x == 0:
+        answer_a = ''
+    elif cal_x == 1:
+        answer_a = 'x'
 
-    
-    # return v + ' + ' + str(n)
-
-    return p
+    answer_b = ' ' + '+' + ' ' + str(cal_n)
+    if cal_n == 0:
+        answer_b = ''
+    elif cal_n < 0:
+        answer_b = ' ' + '-' + str(cal_n)[1:-2]
+                
+    return answer_a + answer_b
 
 print(solution("3x + 7 + x"))
 print(solution("x + x + x"))
