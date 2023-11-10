08fa84897583cf1d934fb76d141ab99f922bf40e
HaeunChoi <kclab922@gmail.com>
2023-11-10 22:03:03 +0900

    pro_가운데글자가져오기

--- programmers/lv1/Z_가운데글자가져오기/sol.py
+++ programmers/lv1/Z_가운데글자가져오기/sol.py
@@ -1,7 +1,10 @@
+# 내 코드
 def solution(s):
     return s[(len(s)//2)] if len(s)%2 else s[len(s)//2-1:len(s)//2+1]
 
+# 다른 코드
 def string_middle(str):
     return str[(len(str)-1)//2 : len(str)//2 + 1]
-    
+
+print(solution('abcde'))
 print(solution('qwer'))
