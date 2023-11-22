4abf7768cf244031dc30578a21c28e3220f3b459
HaeunChoi <kclab922@gmail.com>
2023-11-23 05:57:45 +0900

    pro_키패드누르기

--- programmers/lv1/이상한문자만들기/sol.py
+++ programmers/lv1/이상한문자만들기/sol.py
@@ -1,11 +1,16 @@
 def solution(s):
     answer = ''
-    for w in s.split():
-        for i in range(len(w)):
-            if i % 2 == 0:
-                answer += w[i].upper()
-            else:
-                answer += w[i].lower()
+    for w in s.split(' '):
+        if w == ' ':
+            answer += ' '
+        else: 
+            for i in range(len(w)):  
+                if i % 2:
+                    answer += w[i].lower() 
+                else:
+                    answer += w[i].upper() 
+        answer += ' '
     return answer
 
 print(solution("try hello world"))
+print(solution("try     hello    world"))
--- programmers/lv1/키패드누르기/sol.py
+++ programmers/lv1/키패드누르기/sol.py
@@ -1,15 +1,55 @@
+def distance(start, goal):
+    pad = {
+        1: [0, 0],
+        2: [1, 0],
+        3: [2, 0],
+        4: [0, 1],
+        5: [1, 1],
+        6: [2, 1],
+        7: [0, 2],
+        8: [1, 2],
+        9: [2, 2],
+        '*': [0, 3],
+        0: [1, 3],
+        '#': [2, 3],
+    }
+
+    a1, b1 = pad[start]
+    a2, b2 = pad[goal]
+
+    return abs(a1-a2) + abs(b1-b2)
+
+
+
 def solution(num, hand):
     answer = ''
-    pad = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['*', 0, '#']]
-    for i in range(4):
-        for j in range(3):
-            if j == 0:
+    left = '*'
+    right = '#'
+
+    for i in num:
+        if i in [1, 4, 7]:
+            answer += 'L'
+            left = i
+        elif i in [3, 6, 9]:
+            answer += 'R'
+            right = i
+        else:
+            if distance(left, i) < distance(right, i):
                 answer += 'L'
-            elif j == 2:
+                left = i
+            elif distance(right, i) < distance(left, i):
                 answer += 'R'
+                right = i
             else:
-                if i == 0
-    return 
+                if hand == 'left':
+                    answer += 'L'
+                    left = i
+                else:
+                    answer += 'R'
+                    right = i
+    return answer
+            
+
 
 
 print(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right"))
