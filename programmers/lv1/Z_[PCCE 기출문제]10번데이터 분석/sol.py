038f94c48c9a6db6b98ddc266fccbf535b18e5c6
HaeunChoi <kclab922@gmail.com>
2023-11-29 22:01:09 +0900

    pro_다트게임

--- programmers/lv1/Z_[PCCE 기출문제]10번데이터 분석/sol.py
+++ programmers/lv1/Z_[PCCE 기출문제]10번데이터 분석/sol.py
@@ -1,11 +1,11 @@
 def solution(data, ext, v_ext, srt):
     answer = []
-    extD = {"code":0, "date":1, "maximum":2, "remain":3}
+    mdict = {"code":0, "date":1, "maximum":2, "remain":3}
     
     for d in data:
-        if d[extD[ext]] < v_ext:
+        if d[mdict[ext]] < v_ext:
             answer.append(d)
 
-    return sorted(answer, key = lambda x: x[extD[srt]])
+    return sorted(answer, key = lambda x: x[mdict[srt]])
 
 print(solution([[1, 20300104, 100, 80], [2, 20300804, 847, 37], [3, 20300401, 10, 8]], "date", 20300501, "remain"))
--- programmers/lv1/다트게임/sol.py
+++ programmers/lv1/다트게임/sol.py
@@ -1,9 +1,30 @@
 def solution(dart):
-    # for i in range(len(dart)):
-    #     if dart[i] == 'S':
-            
+    dartSTR = ''
+    for i in dart:
+        dartSTR += i if i.isdigit() else i + ' '
+        dart = dartSTR.split()
+    # dartSTR = '1D # 2S * 3S'
+    # dart = ['1D', '#', '2S', '*', '3S']
+    
+    area = {'S':1, 'D':2, 'T':3}
+    for i in range(len(dart)):
+        if dart[i][-1] in area.keys():
+            dart[i] = int(dart[i][:-1]) ** area[dart[i][-1]]
+        elif dart[i] == '#':
+            dart[i-1] = dart[i-1] * (-1)
+        elif dart[i] == '*':
+            if i == 1:
+                dart[i-1] = dart[i-1] * 2
+            else: 
+                dart[i-1] = dart[i-1] * 2
+                if str(dart[i-2]).isdigit():
+                    dart[i-2] = dart[i-2] * 2
+                else:
+                    dart[i-3] = dart[i-3] * 2
+    # dart = [-2, '#', 4, '*', 3] (연산완료)
 
-    return dart.split('S')
+    return sum([i for i in dart if i not in ['#', '*']])
+    # 숫자만 뽑아서 더하기
 
 
 print(solution('1S2D*3T'))
