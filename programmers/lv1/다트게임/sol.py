f0ef8df2b57b1bb97800f2f3ff179694a381b3a9
HaeunChoi <kclab922@gmail.com>
2023-11-29 22:21:40 +0900

    pro_다트게임_보완

--- programmers/lv1/다트게임/sol.py
+++ programmers/lv1/다트게임/sol.py
@@ -1,9 +1,10 @@
+# 내 코드
 def solution(dart):
-    dartSTR = ''
+    dartCUT = ''
     for i in dart:
-        dartSTR += i if i.isdigit() else i + ' '
-        dart = dartSTR.split()
-    # dartSTR = '1D # 2S * 3S'
+        dartCUT += i if i.isdigit() else i + ' '
+        dart = dartCUT.split()
+    # dartCUT = '1D # 2S * 3S'
     # dart = ['1D', '#', '2S', '*', '3S']
     
     area = {'S':1, 'D':2, 'T':3}
@@ -13,10 +14,8 @@ def solution(dart):
         elif dart[i] == '#':
             dart[i-1] = dart[i-1] * (-1)
         elif dart[i] == '*':
-            if i == 1:
-                dart[i-1] = dart[i-1] * 2
-            else: 
-                dart[i-1] = dart[i-1] * 2
+            dart[i-1] = dart[i-1] * 2
+            if i != 1:
                 if str(dart[i-2]).isdigit():
                     dart[i-2] = dart[i-2] * 2
                 else:
@@ -25,6 +24,34 @@ def solution(dart):
 
     return sum([i for i in dart if i not in ['#', '*']])
     # 숫자만 뽑아서 더하기
+
+
+# 다른 코드
+# 나와 같은 풀이과정은 비슷
+# 1. for문 돌릴 때 '10'이 분리될까봐 10->k 이용 / list.comp 형태 참고
+# 2. dart에 계속 재할당한 나와 달리, 여분의 리스트 answer을 만들어 연산이 비교적 단순화됨
+def solution(dartResult):
+    point = []
+    answer = []
+    dartResult = dartResult.replace('10','k')
+    point = ['10' if i == 'k' else i for i in dartResult]
+    # point = ['1', 'D', '#', '2', 'S', '*', '3', 'S']
+
+    i = -1
+    sdt = ['S', 'D', 'T']
+    for j in point:
+        if j in sdt :
+            answer[i] = answer[i] ** (sdt.index(j)+1)
+        elif j == '*':
+            answer[i] = answer[i] * 2
+            if i != 0 :
+                answer[i - 1] = answer[i - 1] * 2
+        elif j == '#':
+            answer[i] = answer[i] * (-1)
+        else:
+            answer.append(int(j))
+            i += 1
+    return sum(answer)
 
 
 print(solution('1S2D*3T'))
