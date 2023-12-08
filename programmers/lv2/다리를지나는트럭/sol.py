699048dc0274afa8e3b25d3de63f3eab08473d9f
HaeunChoi <kclab922@gmail.com>
2023-12-08 13:32:34 +0900

    pro_다리를지나는트럭

--- programmers/lv2/다리를지나는트럭/sol.py
+++ programmers/lv2/다리를지나는트럭/sol.py
@@ -1,7 +1,35 @@
-def solution(bridge, weight, truck):
-    
+from collections import deque
 
-    return
+def solution(bridge, weight, truck):
+    sec = 0
+    waiting = deque()
+    passing = deque()
+    passed = []
+
+    # 각 트럭별로 경과시간 달아주기
+    for w in truck:
+        waiting.append([1, w])
+
+    # 트럭 이동 시작
+    n = len(truck)
+    while len(passed) < n:
+        sec += 1
+
+        # passing
+        while len(passing) and passing[0][0] == bridge:
+            passed.append(passing.popleft())
+        for i in range(len(passing)):
+            passing[i][0] += 1
+                
+        # waiting
+        if (len(waiting) != 0):
+            sum_w = 0
+            for _, w in passing:
+                sum_w += w
+            if sum_w + waiting[0][1] <= weight:
+                passing.append(waiting.popleft())
+
+    return sec
 
 print(solution(2, 10, [7,4,5,6]))
 print(solution(100, 100, [10]))
