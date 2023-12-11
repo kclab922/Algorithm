c5c681a24ce55f60e22cee282d5a2ee3eb2b7788
HaeunChoi <kclab922@gmail.com>
2023-12-11 12:27:27 +0900

    pro_H-Index

---
+++ programmers/고득점Kit/Sorting/H-Index/sol.py
@@ -0,0 +1,31 @@
+# 이상, 이하 매우 거슬림..
+# 논문별 인용횟수가 아무리 많아도, h-index의 최댓값은 전체 논문 개수 1000개를 넘지 못함.
+
+from collections import deque
+
+def solution(citations):
+    h_max = len(citations)
+    citations.sort(reverse=True)
+    citations = deque(citations)
+
+    count = 0
+    while (count != h_max):
+        if citations[0] >= h_max:
+            citations.popleft()
+            count += 1
+        else:
+            h_max -= 1
+
+    return h_max
+
+
+# 다른 코드
+def solution(citations):
+    citations = sorted(citations)
+    l = len(citations)
+    for i in range(l):
+        if citations[i] >= l-i:
+            return l-i
+    return 0
+
+print(solution([3, 0, 6, 1, 5]))
