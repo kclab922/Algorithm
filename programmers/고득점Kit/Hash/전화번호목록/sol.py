7801a1224a33ed05ba4b2b54d9e47a57a8df19cc
HaeunChoi <kclab922@gmail.com>
2023-12-19 20:21:05 +0900

    pro_전화번호목록

--- programmers/고득점Kit/Hash/전화번호목록/sol.py
+++ programmers/고득점Kit/Hash/전화번호목록/sol.py
@@ -1,4 +1,10 @@
 def solution(pb):
+    pb = sorted(pb, key = lambda x: len(x))
 
-    return
+    for i in range(len(pb)-1):
+        if pb[i] in pb[i+1][]:
+            return False
+    return True
         
+
+print(solution(["119", "97674223", "1195524421"]))
