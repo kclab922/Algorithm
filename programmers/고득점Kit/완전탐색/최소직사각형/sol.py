f49b0ac8e3ca4948d6073bccbe30147daa46b8af
HaeunChoi <kclab922@gmail.com>
2023-12-22 00:57:00 +0900

    pro_최소직사각형

--- programmers/고득점Kit/완전탐색/최소직사각형/sol.py
+++ programmers/고득점Kit/완전탐색/최소직사각형/sol.py
@@ -1,24 +1,25 @@
+# 내 코드 1
+# 각 명함의 가로/세로 중 작은 값들을 모아서 그것들 중 최댓값 * 각 명함의 가로/세로 중 큰 값들을 모아서 그것들 중 최댓값
 def solution(sizes):
-    minSize = sizes[0]
+    return max([sorted(size)[0] for size in sizes]) * max([sorted(size)[1] for size in sizes])
+
+# 내 코드 2
+def solution(sizes):
+    size = sorted(sizes[0])
 
     for i in sizes[1:]: 
-        if i[0] <= minSize[0] and i[1] <= minSize[1]:
-            pass
-        else:
-            if i[0] <= minSize[1] and i[1] <= minSize[0]:
-                pass
-            else:
-                # 지갑사이즈 가로세로 모두에 안 들어가는 경우
-                if min(i) > minSize[0] and min(i) > minSize[1]:
-                    minSize = i
-                # 현재 지갑사이즈 가로세로 중 하나라도 들어가는 경우
-                else:
-                    
+        if min(i) > size[0]:
+            size[0] = min(i)
+        if max(i) > size[1]:
+            size[1] = max(i)
 
-            
+    return size[0] * size[1]
 
 
-    return minSize
+
+
+
+
 
 print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
 print(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]))
