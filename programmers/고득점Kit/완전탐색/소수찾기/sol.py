d0cfc996d0463cf4c796eb67ec6e979bd42e8a51
HaeunChoi <kclab922@gmail.com>
2023-12-21 13:01:37 +0900

    pro_피로도

--- programmers/고득점Kit/완전탐색/소수찾기/sol.py
+++ programmers/고득점Kit/완전탐색/소수찾기/sol.py
@@ -29,3 +29,18 @@ def solution(numbers):
     
 print(solution('17'))
 print(solution('011'))
+
+
+# 에라토스테네스의 체
+
+
+# 다른 코드
+from itertools import permutations
+def solution(n):
+    a = set()
+    for i in range(len(n)):
+        a |= set(map(int, map("".join, permutations(list(n), i + 1))))
+    a -= set(range(0, 2))
+    for i in range(2, int(max(a) ** 0.5) + 1):
+        a -= set(range(i * 2, max(a) + 1, i))
+    return len(a)
---
+++ programmers/고득점Kit/완전탐색/피로도/sol.py
@@ -0,0 +1,22 @@
+from itertools import permutations
+
+def solution(k, d):
+    for i in range(len(d), 1, -1):
+        for j in list(permutations(d, i)): # list = [([80, 20], [50, 40], [30, 10]), ([80, 20], [30, 10], [50, 40]), ... ]
+            k2 = k
+            count = 0
+            for n in j: # j = ([80, 20], [50, 40], [30, 10])
+                if k2 >= n[0]:
+                    k2 -= n[1]
+                    count += 1
+                else:
+                    break
+            if count == len(j):
+                return len(j)
+    return 0
+            
+
+
+
+
+print(solution(80, [[80,20],[50,40],[30,10]]))
