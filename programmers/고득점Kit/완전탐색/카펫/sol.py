744733a912b3f4ba051a9137886ed3a3065f69a7
HaeunChoi <kclab922@gmail.com>
2023-12-26 08:11:13 +0900

    pro_카펫

--- programmers/고득점Kit/완전탐색/카펫/sol.py
+++ programmers/고득점Kit/완전탐색/카펫/sol.py
@@ -1,28 +1,11 @@
 def solution(brown, yellow):
-    total = brown + yellow
 
-    # total의 약수 순서쌍 (가로a, 세로b) 중 a>=b인 경우만 추출
-    comb = []
-    for i in range(1, int(total**0.5+1)): # range(1, 4)
-        if total % i == 0:
-            comb.append([total//i, i]) 
-            # [[9, 1], [3, 3]]
-    
     for i in range(1, int(yellow**0.5+1)):
         if yellow % i == 0:
-            pass
-     
-    for a, b in comb:
-
-
-      
-       
-
+            a, b = yellow//i, i
+            if (a+2) * (b+2) - yellow == brown:
+                return [a+2, b+2]
     
-
-
-    return comb
-
 print(solution(10, 2))
 print(solution(8, 1))
 print(solution(24, 24))
