ad1268c10752b0a18826c6b70e931330f089b89f
HaeunChoi <kclab922@gmail.com>
2023-12-29 10:35:05 +0900

    pro_모음사전

---
+++ programmers/고득점Kit/완전탐색/모음사전/sol.py
@@ -0,0 +1,13 @@
+from itertools import permutations
+
+def solution(word):
+    loc = 0
+    for i in range(1, 6, 1):
+        myL = list(permutations([ 'A', 'E', 'I', 'O', 'U'], i))
+        if 
+
+
+print(solution("AAAAE"))
+# print(solution("AAAE"))
+# print(solution("I"))
+# print(solution("EIO"))
