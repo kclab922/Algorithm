325d81f91807742f9e86e323fb838b469bbc6a4a
HaeunChoi <kclab922@gmail.com>
2024-01-02 09:12:46 +0900

    pro_모음사전

--- programmers/고득점Kit/완전탐색/모음사전/sol.py
+++ programmers/고득점Kit/완전탐색/모음사전/sol.py
@@ -1,10 +1,14 @@
 from itertools import permutations
 
 def solution(word):
-    loc = 0
+    loc = 1
     for i in range(1, 6, 1):
-        myL = list(permutations([ 'A', 'E', 'I', 'O', 'U'], i))
-        if 
+        for j in list(permutations(['A', 'E', 'I', 'O', 'U'], i)):
+            if ''.join(j) == word:
+                return j
+            else:
+                loc += 1
+    return j
          
 
 print(solution("AAAAE"))
