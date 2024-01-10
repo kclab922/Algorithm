85bb01bd8db5c449efb4ff33253f68ee4c2fca93
HaeunChoi <kclab922@gmail.com>
2024-01-10 23:57:59 +0900

    pro_게임맵최단거리

--- programmers/고득점Kit/BFS_DFS/게임맵최단거리/sol.py
+++ programmers/고득점Kit/BFS_DFS/게임맵최단거리/sol.py
@@ -1,18 +1,42 @@
+# def solution(maps):
+#     n = len(maps)
+#     m = len(maps[0])
+#     x, y, count = 0, 0, 0
+
+#     while :
+    
+#         if maps[n-1][m] == 0 and maps[n][m-1] == 0 and maps[n-1][m-1] == 0:
+#             return -1
+#         elif:
+            
+
+
+from collections import deque
+
+def bfs(start, maps):
+    dirs = [(0,1), (1,0), (0,-1), (-1,0)]
+    queue = deque()
+    queue.append(start)
+    while queue:
+        y, x, count = queue.popleft()
+        maps[y][x] = 0
+        for dy, dx in dirs:
+            ny, nx = y+dy, x+dx
+
+            if (ny == len(maps)-1) and (nx == len(maps[0])-1):
+                return count+1
+
+            elif (0 <= ny < len(maps)) and (0 <= nx < len(maps[0])) and (maps[ny][nx] == 1):
+                maps[ny][nx] = 0
+                queue.append((ny, nx, count+1))
+
+    return -1
+
+
 def solution(maps):
-    n = len(maps)
-    m = len(maps[0])
-    x, y, count = 0, 0, 0
-    mini = n*m
-    
-    if maps[n-1][m] == 0 and maps[n][m-1] == 0 and maps[n-1][m-1] == 0:
-        return -1
-    elif:
-        while x = n and y = m and 
+    return bfs((0,0,1), maps)
 
 
-    
-
-    
 
 print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
 print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))
