7406577898c54cec7e19ffb533baeaf325c23902
HaeunChoi <kclab922@gmail.com>
2024-01-24 17:05:03 +0900

    pro_구명보트

--- programmers/고득점Kit/Greedy/구명보트/sol.py
+++ programmers/고득점Kit/Greedy/구명보트/sol.py
@@ -1,12 +1,75 @@
+# 정확성테스트 전부 통과 / 효율성테스트 모두 시간초과
+# def solution(people, limit):
+#     answer = 0
+#     people.sort() 
+
+#     while people:
+#         saved = people.pop() 
+#         answer += 1
+
+#         for person in people:
+#             if person + saved <= limit:
+#                 people.remove(person)
+#                 break
+        
+#     return answer
+
+
+# 답은 모두 맞으나, 시간초과 
+# def solution(people, limit):
+#     answer = 0
+#     people.sort() # 50 50 70 
+
+#     while people:
+#         saved = people.pop() # 80
+#         print(saved)
+#         answer += 1
+
+#         count = 0 # 1
+#         while count < len(people): # 3
+#             if people[-1] + saved <= limit:
+#                 people.pop()
+#                 break
+#             else:
+#                 people = [people.pop()] + people
+#                 count += 1
+
+#         people.sort()
+        
+#     return answer
+
+
+
+
+# from collections import deque
+# def solution(people, limit):
+#     boat = 0
+#     people = deque(sorted(people))
+
+#     while people:
+#         saved = people.pop()
+#         boat += 1
+
+#         if (people) and (saved + people[0] <= limit):
+#             people.popleft()
+
+#     return boat
+
+
+from collections import deque
 def solution(people, limit):
-    
+    boat = 0
+    people = deque(sorted(people))
+
     while people:
-        people.sort()
-        person = people.pop()
-        if person + people[-1] <= limit:
-            
+        if len(people) == 1:
+            return boat + 1
+        else:
+            if (people.pop() + people[0] <= limit):
+                people.popleft()
+            boat += 1
 
-
+    return boat
 
 print(solution([70, 50, 80, 50], 100))
 print(solution([70, 80, 50], 100))
