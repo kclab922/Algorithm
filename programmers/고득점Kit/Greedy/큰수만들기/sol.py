99f1b658bae8cca945e2223a343ac1b4fd5cdd7d
HaeunChoi <kclab922@gmail.com>
2024-01-30 08:50:19 +0900

    pro_큰수만들기

--- programmers/고득점Kit/Greedy/큰수만들기/sol.py
+++ programmers/고득점Kit/Greedy/큰수만들기/sol.py
@@ -1,8 +1,23 @@
 def solution(number, k):
 
-    
-    
-    return       
+    # stack에 입력값을 순서대로 삽입 
+    stack = [number[0]]
+
+    for num in number[1:]:
+        # 들어오는 값이 stack 값보다 크면, 기존의 값을 제거하고 새로운 값으로 바꿈 
+        while stack and stack[-1] < num and k > 0:
+            # 값을 한개 빼서 k에서 1 제거 
+            k -= 1
+            # 내부의 값을 제거 
+            stack.pop()
+        # 새로운 값을 삽입 
+        stack.append(num)
+
+    # 만일 충분히 제거하지 못했으면 남은 부분은 단순하게 삭제
+    # 이렇게 해도 되는 이유는 이미 큰 수부터 앞에서 채워넣었기 때문 
+    if k != 0:
+        stack = stack[:-k]
+    return ''.join(stack)
 
 
 print(solution("1924", 2))
