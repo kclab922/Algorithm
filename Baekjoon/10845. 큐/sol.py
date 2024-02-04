4869125f6f7a41815e9dafc5d75513ef17f37fa0
HaeunChoi <kclab922@gmail.com>
2024-02-05 06:55:54 +0900

    bj_10845.큐

---
+++ Baekjoon/10845. 큐/sol.py
@@ -0,0 +1,22 @@
+from collections import deque
+import sys
+sys.stdin = open('input.txt') 
+
+TC = int(sys.stdin.readline())
+
+q = deque()
+for _ in range(TC):
+    command = sys.stdin.readline()
+
+    if 'push' in command:
+        q.append(int(command[5:]))
+    elif 'pop' in command:
+        print(q.popleft()) if q else print(-1)
+    elif 'size' in command:
+        print(len(q))
+    elif 'empty' in command:
+         print(0) if q else print(1)
+    elif 'front' in command:
+        print(q[0]) if q else print(-1)
+    else:
+        print(q[-1]) if q else print(-1)
